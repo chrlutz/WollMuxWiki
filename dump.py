befd12d07c277f9b7f460b585e34c2503f07bb8b
Christoph Lutz <chrlutz@gmail.com>
2015-02-07 10:21:57 +0100

    dump.py now retrieves articles from Spezial:Alle_Seiten

--- dump.py
+++ dump.py
@@ -4,63 +4,26 @@ import urllib2
 import xml.etree.ElementTree as ET
 
 WIKI="http://www.wollmux.net/wiki"
+ALL_PAGES_URL=WIKI + "/Spezial:Alle_Seiten"
 
-ARTICLES="\
-1._WollMux_Community_Workshop \
-Bedienelemente_des_WollMux \
-Bekannte_Probleme_mit_WollMux-Releases \
-Community-Mitglieder_und_Ansprechpartner \
-Configuration_file_wollmux.conf \
-Configuring_WollMux \
-Datenbanken_mit_dem_WollMux_verwenden \
-Dokumentkommandos_des_WollMux \
-Download \
-Download_WollMux \
-Erstellung_oder_Anpassung_einer_Vorlage_-_Beispiel \
-Features \
-Fehler_im_WollMux_melden \
-Format_von_WollMux-Config-Dateien \
-Formatting_WollMux_config_files \
-Formatvorlagen \
-FormularMax_4000 \
-Fragen_rund_um_Datenquellen \
-Funktionen_des_WollMux_in_der_Praxis \
-Funktionen_im_WollMux_-_Workshop \
-Hauptseite \
-Hilfen_für_Sachleitende_Verfügungen_verwenden \
-Installing_WollMux \
-Konfiguration_des_WollMux_in_Ihrem_Referat \
-Konfigurationsdatei_wollmux.conf \
-Mailinglisten \
-Main_Page \
-Makro_Kochbuch \
-Neuer_Name_für_den_WollMux \
-News \
-Rebranding \
-Repository \
-Schnittstellen_des_WollMux_für_Experten \
-Seriendruck,_erweiterte_Funktion \
-Seriendruck_mit_dem_WollMux \
-Seriendruckvorlagen_mit_dem_WollMux_erstellen \
-SimpleConfig \
-Standardkonfiguration_des_WollMux \
-Standardkonfiguration_des_WollMux_downloaden \
-Standardkonfiguration_des_WollMux_installieren_und_pflegen \
-Text_fragments_in_wollmux \
-Textfragmente_im_WollMux \
-WollMux \
-WollMux-Praktikertag_am_18.11.2010 \
-WollMux-Schulungen \
-WollMuxBar \
-WollMux_Komfortdruck_geht_nicht_(Problem_und_Lösung) \
-WollMux_document_commands \
-WollMux_downloaden \
-WollMux_installieren \
-WollMux_kompilieren \
-WollMux_mit_LibreOffice_Portable \
-Wollmux-Debugging"
+# reading article overview
+response = urllib2.urlopen(ALL_PAGES_URL)
+xml = ET.fromstring( response.read() )
+anchors = xml.findall("body/" 
+	+ "div[@id='content']/"
+	+ "div[@id='bodyContent']/"
+	+ "div[@id='mw-content-text']/"
+	+ "table[@class='mw-allpages-table-chunk']/"
+	+ "tr/td/a"
+)
+articles = []
+for link in anchors:
+	article = urllib2.unquote( link.get('href') )
+	article = article[6:] # strip away "/wiki/"
+	articles.append(article)
 
-for i in ARTICLES.split():
+# fetch articles
+for i in articles:
 	print "fetching " + i
 	response = urllib2.urlopen(WIKI + '/api.php?action=query&prop=revisions&rvprop=content&format=xml&titles=' + i)
 	xml = ET.fromstring( response.read() )
